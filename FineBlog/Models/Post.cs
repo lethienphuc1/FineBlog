1a556b14689cb99d6a1f46a4de6e945fa58ccd9a
lethienphuc1 <157466550+lethienphuc1@users.noreply.github.com>
2024-11-17 10:42:26 +0700

    Add project files.

---
+++ FineBlog/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace FineBlog.Models
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public string? Tiltle { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? ApplicationUserId { get; set; }
+        public ApplicationUser? ApplicationUser { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string? Description { get; set; }
+        public string? Slug { get; set; }
+
+    }
+}
